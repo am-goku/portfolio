d03986451acbdaf7c07c7468008a85e1c9f549a3
Gokul Krishna <amkrishna2483@gmail.com>
2023-12-21 16:07:41 +0530

    icon added, and updated metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,12 +3,35 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import Header from './layouts/Header'
 
+import icn from "@/app/favicon.ico"
+
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: 'Gokul Krishna',
-  description: 'Portfolio Website',
-}
+  title: "Gokul Krishna",
+  description: "Portfolio Website",
+  category: "Web Development",
+  keywords: [
+    "MERN",
+    "Web Developer",
+    "Portfolio",
+    "Javascript",
+    "HTML",
+    "CSS",
+    "React",
+    "Next",
+    "MongoDB",
+    "Node",
+    "Expressjs",
+    "Full Stack Developer",
+    "Gokul Krishna",
+    "Gokul Krishna G H",
+    "Relink",
+    "Trendly",
+    "Brototype",
+    "Developer"
+  ],
+};
 
 export default function RootLayout({
   children,
@@ -17,6 +40,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href={icn.src} />
+      </head>
       <body className={inter.className}>
         <main className="flex flex-col gap-10" draggable={false}>
           <div className="">
